d996a96a8abd550c253d6804a318e05bb6b649e5
Almaz Murzabekov <almaz@murzabekov.net>
2025-08-01 16:56:16 +0200

    fix: remove unused components

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -1,4 +1,2 @@
 export { LoadingSpinner } from './loading-spinner';
 export { ErrorDisplay } from './error-display';
-export { PageWrapper } from './page-wrapper';
-export { DataStateHandler } from './data-state-handler';
